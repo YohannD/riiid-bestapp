d391e6311163f16dbf0bb460a8f325ccdb767497
Yohann Doillon <yohann.doillon@gmail.com>
2020-12-17 12:52:32 +0100

    done

--- Heroku/data.py
+++ Heroku/data.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 from Heroku.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import HistGradientBoostingClassifier
-
+#'papate'
 
 DIST_ARGS = dict(start_lat="pickup_latitude",
                  start_lon="pickup_longitude",
