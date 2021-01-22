3109e71c3b06a7212005aff4ac9e2f0ebf8c0623
Yohann Doillon <yohann.doillon@gmail.com>
2021-01-22 18:06:31 +0100

    some commit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo '\
 [general]\n\
 email = \"<myemail@domain>\"\n\
-" > ~/.streamlit/credentials.toml
+' > ~/.streamlit/credentials.toml
 
-echo "\
+echo '\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+' > ~/.streamlit/config.toml
