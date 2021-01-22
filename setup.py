d898a408d9a7d434993a61abfe5b8728716ee44d
Yohann Doillon <yohann.doillon@gmail.com>
2021-01-22 17:52:26 +0100

    another commit

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content]
 
-setup(name="streamlitapp",
+setup(name="riiid-app",
       version="1.0",
-      description="streamlit app",
+      description="riiid-app",
       packages=find_packages(),
       include_package_data=True,
       install_requires=requirements)
